06083d26cb6d73e3d3ed859cad0bc94a016f1ae8
canozben <canozben@windowslive.com>
2023-08-12 17:07:17 +0300

    fixed lucide-react module not found

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import { ShoppingBag } from 'lucide-react';
 import { ShoppingBasket } from 'lucide-react';
-import { ShoppingCart } from 'lucide-react';
 import Button from "./ui/button";
 import { useEffect,useState } from 'react';
 
@@ -19,9 +17,7 @@ const NavbarActions = () => {
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
-        <ShoppingBag size={20} color="black"/>
-        <ShoppingBasket />
-        <ShoppingCart />
+        <ShoppingBasket size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           0
         </span>
